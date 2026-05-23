f0e97334ace769f61b4630c32db54e24f36d3aa0
taulde <hello.taulde@gmail.com>
2026-05-23 21:16:47 +0300

    Add links.js
    Connect links for lesson's button

---
+++ js/links.js
@@ -0,0 +1,15 @@
+let openOAP = () => {
+    window.open('https://docs.google.com/spreadsheets/d/19iaCrvmTUjUjjkYqaOqoV_aFo2_cqRQOt-8d48YVUSM/edit?gid=301514836#gid=301514836');
+}
+
+let openIT = () => {
+    window.open('https://docs.google.com/spreadsheets/d/1N_d-iy3EtzM0oYz1xjKwvV64ob-fzGdMkOU32lY7rTU/edit?gid=0#gid=0');
+}
+
+let openCS = () => {
+    window.open('https://docs.google.com/spreadsheets/d/1HxhebF2rJffdIZpvA99flgmsz_C4V6ma/edit?usp=drive_link&ouid=108020577343891577171&rtpof=true&sd=true');
+}
+
+let openCK = () => {
+    window.open('https://docs.google.com/spreadsheets/d/1zYnwUj3puqc9bMG7wt5IfPddreOcukXF4pOqSr1Qf1I/edit?gid=820758426#gid=820758426');
+}
